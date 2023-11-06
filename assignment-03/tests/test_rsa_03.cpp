0cffcdbb62f0587035905d22571e9da177ce85f7
j1nxie <rylieeeeexd@gmail.com>
2023-11-06 22:54:04 +0700

    refactor(test): add assertions

--- assignment-03/tests/test_rsa_03.cpp
+++ assignment-03/tests/test_rsa_03.cpp
@@ -12,5 +12,9 @@ int main() {
     fmt::print("Public key: n = {}, e = {}\n", public_key.n, public_key.e);
     fmt::print("Private key: n = {}, d = {}\n", private_key.n, private_key.d);
 
-    fmt::print("Cipher text: {}\n", RSA::encrypt(5, public_key));
+    uint64_t cipher = RSA::encrypt(5, public_key);
+    fmt::print("Cipher text: {}\n", cipher);
+
+    assert(e == 3);
+    assert(cipher == 26);
 }
